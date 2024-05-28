8b1351ebfd1d29fda0c6367725ff6bf2975dd071
Sujin1517 <11tnwls12@gmail.com>
2024-05-28 12:36:59 +0900

    update controller

--- src/main/java/com/example/user/command/controller/UserController.java
+++ src/main/java/com/example/user/command/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.user.command.controller;
 
+import com.example.user.command.dto.request.UserEditRequest;
 import com.example.user.command.entity.User;
 import com.example.user.command.kafka.dto.KafkaStatus;
 import com.example.user.command.kafka.producer.UserProducer;
@@ -20,23 +21,47 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createUser(){
-
+    public void createUser(@RequestBody User user){
+        KafkaStatus<User> userKafkaStatus = new KafkaStatus<>(user, "insert");
+        kafkaList.add(userKafkaStatus);
+        userProducer.send(user, "insert");
+        kafkaList.remove(userKafkaStatus);
     }
 
     @PutMapping("{id}")
-    public void editUser(){
-
+    public void editUser(
+            @PathVariable String id,
+            @RequestBody User req
+    ){
+        User user = User.builder()
+                .user_id(id)
+                .user_image(req.getUser_image())
+                .user_name(req.getUser_name())
+                .user_desc(req.getUser_desc())
+                .build();
+        KafkaStatus<User> userKafkaStatus = new KafkaStatus<>(user, "insert");
+        kafkaList.add(userKafkaStatus);
+        userProducer.send(user, "update");
+        kafkaList.remove(userKafkaStatus);
     }
 
     @DeleteMapping("{id}")
-    public void deleteUser() {
-
+    public void deleteUser(@PathVariable String id) {
+        User user = User.builder().user_id(id).build();
+        KafkaStatus<User> userKafkaStatus = new KafkaStatus<>(user, "insert");
+        kafkaList.add(userKafkaStatus);
+        userProducer.send(user, "delete");
+        kafkaList.remove(userKafkaStatus);
     }
 
 
     @Scheduled(cron ="*/10 * * * * *")
     public void reSend() {
-
+        List<KafkaStatus<User>> sucessList = new ArrayList<>();
+        kafkaList.forEach(e -> {
+            userProducer.send(e.data(), e.status());
+            sucessList.add(e);
+        });
+        sucessList.forEach(kafkaList::remove);
     }
 }
--- src/main/java/com/example/user/command/entity/User.java
+++ src/main/java/com/example/user/command/entity/User.java
@@ -1,4 +1,19 @@
 package com.example.user.command.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Builder
 public class User {
+    private String user_id;
+    private String user_image;
+    private String user_name;
+    private String user_desc;
+    private Integer user_total_post;
+    private Integer user_total_like;
+    private Boolean user_disable;
 }
