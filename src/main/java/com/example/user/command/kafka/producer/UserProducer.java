f61005583e201bcc8c085e618249414e72a3e7e7
Sujin1517 <11tnwls12@gmail.com>
2024-05-28 09:43:46 +0900

    create kafka producer

--- src/main/java/com/example/user/command/kafka/producer/UserProducer.java
+++ src/main/java/com/example/user/command/kafka/producer/UserProducer.java
@@ -1,4 +1,27 @@
 package com.example.user.command.kafka.producer;
 
+import com.example.user.command.entity.User;
+import com.example.user.command.kafka.dto.KafkaStatus;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserProducer {
+    private final KafkaTemplate<String, KafkaStatus<User>> kafkaTemplate;
+    @Value("${kafka.topic.name}") private final String topic;
+
+    @Bean
+    private NewTopic newTopic() {
+        return new NewTopic(topic, 1, (short) 1);
+    }
+
+    public void send(User user, String status) {
+        KafkaStatus<User> kafkaStatus = new KafkaStatus<>(user, status);
+        kafkaTemplate.send(topic, kafkaStatus);
+    }
 }
